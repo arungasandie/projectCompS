a9ae9a9e28cef652e110530350a72846a07571e1
Sandraaa <awuor.arunga@gmail.com>
2021-05-01 15:56:46 +0300

    created index and products page
    index, changed views, products page and aligned navbar to the right

--- PROJECT_C/mywebsite/views.py
+++ PROJECT_C/mywebsite/views.py
@@ -3,5 +3,13 @@ from flask import Blueprint, render_template
 views = Blueprint('views', __name__)
 
 @views.route('/')
+def welcome():
+    return render_template("index.html")
+
+@views.route('/home')
 def home():
     return render_template("home.html")
+
+@views.route('/products')
+def products():
+    return render_template("products.html")
