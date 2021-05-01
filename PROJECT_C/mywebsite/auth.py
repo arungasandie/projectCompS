eb28ed677ab9bc7042ff6a7368e2274820ab9325
Sandraaa <awuor.arunga@gmail.com>
2021-05-01 21:39:46 +0300

    database
    created database, added contact to sign up and verification of signup

--- PROJECT_C/mywebsite/auth.py
+++ PROJECT_C/mywebsite/auth.py
@@ -4,6 +4,7 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
+    data = request.form
     return render_template("login.html")
 
 @auth.route('/logout')
@@ -12,4 +13,24 @@ def logout():
 
 @auth.route('/sign-up',methods=['GET','POST'])
 def signup():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        email = request.form.get('email')
+        contact = request.form.get('contact')
+        password1 = request.form.get('password')
+        username = request.form.get('password2')
+        usertype = request.form.get('usertype')
+        
+        if len(email) < 4:
+            pass
+        elif len(username) < 2:
+            pass
+        elif password1 != password2:
+            pass
+        elif len(password) < 7:
+            pass
+        elif len(contact) < 8:
+            pass
+        else:
+            pass
     return render_template("signup.html") 
